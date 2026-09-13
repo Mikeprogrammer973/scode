91dc3eb3568d271fb6506f3129c39631b45b80ff
Mike D. Pascal <96785933+Mikeprogrammer973@users.noreply.github.com>
2026-09-13 13:29:21 -0400

    Expand content paths in Tailwind config

--- scode/tailwind.config.js
+++ scode/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/*tsx', './src/*/*tsx', './src/*/*/*tsx'],
+  content: [
+    './src/*tsx', 
+    './src/*/*tsx', 
+    './src/*/*/*tsx',
+    './src/*/*/*/*tsx'
+  ],
   theme: {
     extend: {},
   },
